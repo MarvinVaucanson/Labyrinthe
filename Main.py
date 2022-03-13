0a19198b92d888f83f11404160c34d85832b38d7
MarvinVaucanson <92741855+MarvinVaucanson@users.noreply.github.com>
2022-03-13 11:58:06 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
 #le jeu de labyrinthe
+###====# Bibliotheque #====###
+
+import pygame
+from pygame.locals import *
+from random import randint, choice
+from sys import exit
+import matplotlib.pyplot as plt
+from pile import Pile
 
 ###====# Class #====###
 
-
+class Jeu :
+    pass
 
 
 
